06affc88fecefc5fbbc731afb498d9140ccdef77
AnthonyBouillon <anthony-bouillon@outlook.fr>
2019-03-13 16:43:01 +0100

    Manipuler une API AJAX JSON

--- phase_4/index.php
+++ phase_4/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'list_reg.php';
 ?>
 <!DOCTYPE html>
@@ -13,21 +13,23 @@ include 'list_reg.php';
     <body>
         <form method="post" action="post.php" >
         <select name="dep_reg_id" id="dep_reg_id">
-            <?php 
+            <?php
             foreach($result as $row){ ?>
                 <option value="<?= $row->reg_id ?>"><?= $row->reg_v_nom ?></option>
-            <?php } ?> 
+            <?php } ?>
         </select>
         </form>
- 
+
         <div id="div1"></div>
         <!--  Attention : Jquery sans slim sinon Ajax ne fonctionne pas -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script>
             $('#dep_reg_id').change(function () {
-                $('#div1').html('');
+            $('#div1').html('');
                 $.post({
                     //Fichier où se trouve les données à récupérer
                     url: "post.php",
@@ -41,12 +43,12 @@ include 'list_reg.php';
                     success: function (resultat) {
                         var contenu = '';
                         // On parcourt l'objet
-                        $.each(resultat, function (key, val) {
+                        $.each(resultat, function (inutile, value) {
                             // et on ajoute les noms des départements dans une variable
-                            contenu += val.dep_nom + "<br>";
+                            contenu += value.dep_nom + "<br>";
                         });
                         // Affiche le résultat
-                        $("#div1").html(contenu); 
+                        $("#div1").html(contenu);
                     },
                     // Si la requête ajax n'a pas fonctionné, message d'erreur
                     error: function(){
@@ -54,7 +56,12 @@ include 'list_reg.php';
                     }
                 });
             });
+
+
+
+
         </script>
+
     </body>
 
 </html>
